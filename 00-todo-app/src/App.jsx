fa055c4cd28cd88b8a0a9c9724621772d8900816
mhShohan <mehdihasanshohan25@gmail.com>
2023-04-15 00:07:25 +0600

    start

--- 00-todo-app/src/App.jsx
+++ 00-todo-app/src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1>Todo App</h1>
     </div>
   )
 }
