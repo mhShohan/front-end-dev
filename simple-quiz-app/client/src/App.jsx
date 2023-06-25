b3f1bf44efde8952f6373385e391505af814d03f
mhshohan1 <mehdihasanshohan25@gmail.com>
2023-06-25 22:58:53 +0600

    up

--- simple-quiz-app/client/src/App.jsx
+++ simple-quiz-app/client/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { Step, Stepper } from 'react-form-stepper';
 import Quiz from './components/Quiz';
 import QuestionContextProvider from './context';
 
@@ -8,15 +7,6 @@ const App = () => {
 
   return (
     <div className='center'>
-      {/* <Stepper activeStep={step}>
-        <Step label='Children Step 1' />
-        <Step label='Children Step 2' />
-        <Step label='Children Step 3' />
-        <Step label='Children Step 4' />
-        <Step label='Children Step 5' />
-      </Stepper>
-      <button onClick={() => setStep((p) => p - 1)}>prev step</button>
-      <button onClick={() => setStep((p) => p + 1)}>next step</button> */}
       <QuestionContextProvider>
         <Quiz />
       </QuestionContextProvider>
