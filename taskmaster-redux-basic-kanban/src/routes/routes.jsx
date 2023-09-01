4c052c709f7a86b8fa6f81c7a490e67c589841d3
mhshohan1 <mehdihasanshohan25@gmail.com>
2023-09-01 23:15:40 +0600

    simple fix

--- taskmaster-redux-basic-kanban/src/routes/routes.jsx
+++ taskmaster-redux-basic-kanban/src/routes/routes.jsx
@@ -4,6 +4,7 @@ import Tasks from '../pages/Tasks';
 import Chat from '../pages/Chat';
 import Settings from '../pages/Settings';
 import Profile from '../pages/Profile';
+
 const routes = createBrowserRouter([
   {
     path: '/',
