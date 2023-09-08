d52fc3b4028d608d28cc69e5adaa2896058748f2
mhshohan1 <mehdihasanshohan25@gmail.com>
2023-09-08 23:48:33 +0600

    Intro to the TEST Driven Development

--- react-testing-ts-1/src/components/greet/Greet.test.tsx
+++ react-testing-ts-1/src/components/greet/Greet.test.tsx
@@ -4,6 +4,12 @@ import Greet from './Greet';
 
 test('Greet render correctly', () => {
   render(<Greet />);
-  const textElement = screen.getByText('Greet');
+  const textElement = screen.getByText('Hello, !');
+  expect(textElement).toBeInTheDocument();
+});
+
+test('Greet render with name', () => {
+  render(<Greet name='Shohan' />);
+  const textElement = screen.getByText('Hello, Shohan!');
   expect(textElement).toBeInTheDocument();
 });
--- react-testing-ts-1/src/components/greet/Greet.tsx
+++ react-testing-ts-1/src/components/greet/Greet.tsx
@@ -1,5 +1,9 @@
-const Greet = () => {
-  return <div>Greet</div>;
+interface IGreet {
+  name?: string;
+}
+
+const Greet = ({ name }: IGreet) => {
+  return <div>Hello, {name}!</div>;
 };
 
 export default Greet;
