7ea32a3664164c64ff3a796a4b2f482b1640066f
mhshohan1 <mehdihasanshohan25@gmail.com>
2023-09-09 14:50:16 +0600

    RTL query - getByRole

--- react-testing-ts-1/src/__test__/Application.test.tsx
+++ react-testing-ts-1/src/__test__/Application.test.tsx
@@ -1,10 +1,35 @@
 import { render, screen } from '@testing-library/react';
 import Application from '../components/application/Application';
 
-// describe('Application', () => {
-//   test.only('render correctly', () => {
-//     render(<Application />);
-//     const nameElement = screen.getByRole('textbox');
-//     expect(nameElement).toBeInTheDocument();
-//   });
-// });
+describe('Application', () => {
+  test('render correctly', () => {
+    render(<Application />);
+
+    // render h1
+    const headingOne = screen.getByRole('heading', { level: 1 });
+    expect(headingOne).toBeInTheDocument();
+
+    // render h3
+    const headingTwo = screen.getByRole('heading', { level: 3 });
+    expect(headingTwo).toBeInTheDocument();
+
+    // render input
+    const nameElement = screen.getByRole('textbox', { name: 'Name' });
+    expect(nameElement).toBeInTheDocument();
+
+    // render input
+    const descriptionElement = screen.getByRole('textbox', {
+      name: 'Description',
+    });
+    expect(descriptionElement).toBeInTheDocument();
+
+    const locationElement = screen.getByRole('combobox');
+    expect(locationElement).toBeInTheDocument();
+
+    const termsElement = screen.getByRole('checkbox');
+    expect(termsElement).toBeInTheDocument();
+
+    const submitBtnElement = screen.getByRole('button');
+    expect(submitBtnElement).toBeInTheDocument();
+  });
+});
--- react-testing-ts-1/src/components/application/Application.tsx
+++ react-testing-ts-1/src/components/application/Application.tsx
@@ -1,28 +1,36 @@
 const Application = () => {
   return (
-    <form>
-      <div>
-        <label htmlFor='name'>Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div>
-        <label htmlFor='location'>Location</label>
-        <select id='location'>
-          <option value=''>Select a country</option>
-          <option value='bangladesh'>bangladesh</option>
-          <option value='india'>india</option>
-          <option value='pakistan'>pakistan</option>
-          <option value='nepal'>nepal</option>
-          <option value='bhutan'>bhutan</option>
-        </select>
-      </div>
-      <div>
-        <label>
-          <input type='checkbox' id='terms' /> Accept terms and conditions!
-        </label>
-      </div>
-      <button>Submit</button>
-    </form>
+    <div>
+      <h1>Application From</h1>
+      <h3>Register an application for get update</h3>
+      <form>
+        <div>
+          <label htmlFor='name'>Name</label>
+          <input type='text' id='name' />
+        </div>
+        <div>
+          <label htmlFor='description'>Description</label>
+          <input type='text' id='description' />
+        </div>
+        <div>
+          <label htmlFor='location'>Location</label>
+          <select id='location'>
+            <option value=''>Select a country</option>
+            <option value='bangladesh'>bangladesh</option>
+            <option value='india'>india</option>
+            <option value='pakistan'>pakistan</option>
+            <option value='nepal'>nepal</option>
+            <option value='bhutan'>bhutan</option>
+          </select>
+        </div>
+        <div>
+          <label>
+            <input type='checkbox' id='terms' /> Accept terms and conditions!
+          </label>
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
   );
 };
 
